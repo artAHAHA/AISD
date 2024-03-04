3c88d8069ea435a8d722be899fc8a983332c18a2
belokurov_a_i <belokurovarturka@gmail.com>
2024-03-04 10:34:57 +0300

    1 task

--- src/Task_1_23/Main.java
+++ src/Task_1_23/Main.java
@@ -1,12 +1,14 @@
 package Task_1_23;
 
+import Task_1_23.trafficLight.TrafficSignalWithArrow;
+
 public class Main {
     public static void main(String[] args) {
         int[] intervals = {1000, 2000, 2000, 4000, 3000};
 
         //TrafficLight.BasicTrafficLight basicTrafficLight = new TrafficLight.BasicTrafficLight(intervals);
         //TrafficLight.TrafficSignal trafficSignal = new TrafficLight.TrafficSignal(intervals);
-        TrafficLight.TrafficSignalWithArrow trafficSignalWithArrow = new TrafficLight.TrafficSignalWithArrow(intervals);
+        TrafficSignalWithArrow trafficSignalWithArrow = new TrafficSignalWithArrow(intervals);
 
         //basicTrafficLight.start();
         //trafficSignal.start();
