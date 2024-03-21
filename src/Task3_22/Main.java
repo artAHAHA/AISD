ec0f6b8144e4231d316f394baa8e6cecd6bb7a62
belokurov_a_i <belokurovarturka@gmail.com>
2024-03-21 09:13:55 +0300

    3 task

--- src/Task3_22/Main.java
+++ src/Task3_22/Main.java
@@ -3,8 +3,20 @@ package Task3_22;
 import Task3_22.networkPrinter.NetworkPrinter;
 import Task3_22.networkPrinter.Print;
 
+import java.util.PriorityQueue;
+
 public class Main {
     public static void main(String[] args) {
-        Print.createTasks();
+        NetworkPrinter task = new NetworkPrinter(10, 4, 2, 2);
+        NetworkPrinter task1 = new NetworkPrinter(1, 4, 1, 4);
+        NetworkPrinter task2 = new NetworkPrinter(14, 2, 1, 4);
+
+
+        PriorityQueue<NetworkPrinter> tasks = new PriorityQueue<NetworkPrinter>();
+
+        tasks.add(task);
+        tasks.add(task1);
+        tasks.add(task2);
+        Print.print(tasks);
     }
 }
--- src/Task3_22/networkPrinter/Print.java
+++ src/Task3_22/networkPrinter/Print.java
@@ -8,21 +8,28 @@ public class Print {
 
         NetworkPrinter task = new NetworkPrinter(10, 4, 2, 2);
         NetworkPrinter task1 = new NetworkPrinter(1, 4, 1, 4);
+        NetworkPrinter task2 = new NetworkPrinter(1, 2, 1, 4);
+
 
         PriorityQueue<NetworkPrinter> tasks = new PriorityQueue<NetworkPrinter>();
 
         tasks.add(task);
         tasks.add(task1);
-
-        System.out.println(tasks.poll().time + " - " + (task.time + task.pages));
-        System.out.println(tasks.poll().time);
-    }
-
-/*    public void print(){
+        tasks.add(task2);
 
     }
 
-    private PriorityQueue<NetworkPrinter> modeling(){
+    public static void print(PriorityQueue<NetworkPrinter> tasks) {
 
-    }*/
+        int currentTime = 0;
+
+        while (!tasks.isEmpty()) {
+            NetworkPrinter currentTask = tasks.poll();
+            if (currentTask.time >= currentTime) {
+                System.out.println(currentTask.time + " - " + (currentTask.time + currentTask.pages));
+            }
+            currentTime = currentTask.time + currentTask.pages;
+        }
+    }
+
 }
